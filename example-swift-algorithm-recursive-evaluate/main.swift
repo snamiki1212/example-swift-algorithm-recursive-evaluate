1d9c62b5fabb97e9c78f8b14618e059ccad51f38
snamiki1212 <snamiki1212@gmail.com>
2021-07-27 13:26:46 -0700

    Update error handler

--- example-swift-algorithm-recursive-evaluate/main.swift
+++ example-swift-algorithm-recursive-evaluate/main.swift
@@ -7,10 +7,12 @@
 
 import Foundation
 
+enum MyError: Error {
+    case InvalidCalculator;
+    case InvalidExpression;
+}
 
-
-
-func evaluate(_ expr: String) -> Int {
+public func evaluate(_ expr: String) -> Int {
     if( canBeNum(expr) ) { return Int(expr) ?? -1}
     let (
         calcIdx,
@@ -22,52 +24,43 @@ func evaluate(_ expr: String) -> Int {
     
     let left = evaluate(expr[startIdxOfA, endIdxOfA + 1])
     let right = evaluate(expr[startIdxOfB, endIdxOfB + 1])
-    return calc(expr[calcIdx], left, right);
+    return try! calc(expr[calcIdx], left, right);
 }
 
 func canBeNum(_ str: String) -> Bool {
     return !str.contains("+") && !str.contains("*")
 }
 
-func calc(_ calculator: String, _ left: Int, _ right: Int) -> Int{
+func calc(_ calculator: String, _ left: Int, _ right: Int) throws -> Int {
     if(calculator == "+") {return left + right;}
     if(calculator == "*") {return left * right;}
     
-    print("[Error]Invalid calcolator")
-    return -1
-//    throw Error("Invalid calculator")
+    throw MyError.InvalidCalculator;
 }
 
 func isCalculator(_ str: String) -> Bool {
     return str == "+" || str == "*"
 }
-
-func getIndex(_ expr: String) -> (calcIdx: Int, startIdxOfA: Int, endIdxOfA: Int, startIdxOfB: Int, endIdxOfB: Int) {
     
-    let (calcIdx, endIdxOfA, startIdxOfB): (Int, Int, Int) = {
-        var calcIdx: Int, endIdxOfA: Int, startIdxOfB: Int;
+func getCalcIdx(_ expr: String) throws -> Int{
+    var i = 0;
+    var deep = 0;
+    while(i < expr.count){
+        let isCenterCalculator = deep == 1 && isCalculator(expr[i])
+        if(isCenterCalculator) { return i; }
+        if(expr[i] == "(") { deep+=1; }
+        if(expr[i] == ")") { deep-=1; }
+        i+=1;
+    }
+    
+    throw MyError.InvalidExpression;
+}
 
-        var i = 0;
-        var deep = 0;
-        while(i < expr.count){
-            let isCenterCalculator = deep == 1 && isCalculator(expr[i])
-            if(isCenterCalculator) {
-                endIdxOfA = i - 1
-                calcIdx = i
-                startIdxOfB = i + 1;
-                return(calcIdx, endIdxOfA, startIdxOfB)
-            }
-            if(expr[i] == "(") { deep+=1; }
-            if(expr[i] == ")") { deep-=1; }
-            i+=1;
-        }
-        
-        //        throw Error("Invalid data. it's not full-parenthesis.")
-        print("[Error]Invalid data.", i, deep)
-        return (-1, -1, -1)
-    }()
-        
+func getIndex(_ expr: String)  -> (calcIdx: Int, startIdxOfA: Int, endIdxOfA: Int, startIdxOfB: Int, endIdxOfB: Int) {
+    let calcIdx = try! getCalcIdx(expr);
     let startIdxOfA = 1;
+    let endIdxOfA = calcIdx - 1
+    let startIdxOfB = calcIdx + 1;
     let endIdxOfB = expr.count - 2;
     return (calcIdx: calcIdx, startIdxOfA: startIdxOfA, endIdxOfA: endIdxOfA, startIdxOfB:startIdxOfB, endIdxOfB:endIdxOfB)
 }
@@ -75,9 +68,9 @@ func getIndex(_ expr: String) -> (calcIdx: Int, startIdxOfA: Int, endIdxOfA: Int
 
 //----------------
 var expr: String;
-//expr = "7";
-expr = "(2+2)";
-expr = "((1+3)+((1+10)*5))";
+expr = "7";
+//expr = "(2+2)";
+//expr = "((1+3)+((1+10)*5))";
 
 let result = evaluate(expr)
 print(result);
